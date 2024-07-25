0a0b97650e613cd94d737b69ee1a4c42160fb2ff
at-pyrix <probablynotyasho@gmail.com>
2024-07-25 23:17:44 +0530

    Fix messed up versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dlorg',
-    version='1.0',
+    version='2.0',
     description='A Python script to organize your downloads folder by file extensions and watch for new files.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
